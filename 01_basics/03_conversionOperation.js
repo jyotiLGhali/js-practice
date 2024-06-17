eb1ff362e7caa475b89924e2cfd6faea003e9f6e
jyotiLGhali <132039742+jyotiLGhali@users.noreply.github.com>
2024-06-17 18:59:42 +0000

    Conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false;
+
+//console.log(typeof score)
+let  valueScore = Number(score)
+//console.log(typeof valueScore)
+//console.log(valueScore)
+
+//"33" =>33
+//"33anc" => NaN
+// true => 1
+// false => 0
+//undefined => NaN
+//null => 0
+
+let isLogedIn = 'jyoti'
+let booleanConverterTest = Boolean(isLogedIn)
+//console.log(booleanConverterTest)
+
+//1 => true, 0=> false,
+//"" => false
+//"abc" => true
+
+let someNumber = 89
+
+let covertString = String(someNumber)
+console.log(covertString);
+console.log(typeof covertString)
